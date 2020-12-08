f9b27d661290a76202818c3d7e45221f87f05718
gilshw91 <59819488+gilshw91@users.noreply.github.com>
2020-12-08 17:54:22 +0200

    Updates

---
+++ precache-manifest.d22f92bed9730931367b050e62281a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700b10c4ae3ccb3f604f1f78d389f270",
+    "url": "/dynamicUI-swagger/index.html"
+  },
+  {
+    "revision": "857c668c9a3550c2544f",
+    "url": "/dynamicUI-swagger/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "a75f50185c37165b3e38",
+    "url": "/dynamicUI-swagger/static/css/main.bb1ef5e4.chunk.css"
+  },
+  {
+    "revision": "857c668c9a3550c2544f",
+    "url": "/dynamicUI-swagger/static/js/2.812c8c47.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/dynamicUI-swagger/static/js/2.812c8c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75f50185c37165b3e38",
+    "url": "/dynamicUI-swagger/static/js/main.0252be2a.chunk.js"
+  },
+  {
+    "revision": "2379cfe1281cddb5b0b8",
+    "url": "/dynamicUI-swagger/static/js/runtime-main.e62e5163.js"
+  }
+]);
